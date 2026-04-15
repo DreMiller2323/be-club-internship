f8e50d45afb0ce687ab5b6bde788b517d3a43d5f
Andre Miller <71991034+DreMiller2323@users.noreply.github.com>
2026-04-14 22:43:59 -0700

    Clarify that this file should be page.jsx
    Updated the comment to clarify the file type.

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ import { redirect } from 'next/navigation'
 export default function Home() {
   const sendToSignUp=()=>{
           redirect('/signup')
-
+//this PAGE SHOULD be page.jsx!
   }
   return (
  <div className="image-container">
